e0190ba47019e9b2bcb6be5c7e37448bf9d497cf
jugal <jugalbhatt3@gmail.com>
2020-11-24 23:55:35 +0530

    reusable auth

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user";
 
 const router = express.Router();
@@ -25,17 +26,33 @@ router.post(
 		const { email, password } = req.body;
 
 		const existingUser = await User.findOne({ email });
-		if(existingUser){
-			throw new BadRequestError('Email in use')
+		if (existingUser) {
+			throw new BadRequestError("Email in use");
 		}
-		
+
 		const user = User.build({
 			email,
-			password
-		})
+			password,
+		});
 		await user.save();
 
-		res.status(201).send(user)
+		//Generate JWT
+
+		const userJwt = jwt.sign(
+			{
+				id: user.id,
+				email: user.email,
+			},
+			process.env.JWT_KEY!
+		);
+
+		//Store cookie
+
+		req.session = {
+			jwt: userJwt,
+		};
+
+		res.status(201).send(user);
 	}
 );
 
