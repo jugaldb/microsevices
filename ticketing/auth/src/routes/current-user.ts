7687645a0a3e6a8a6a5ea01c749d8786afd13a3e
jugal <jugalbhatt3@gmail.com>
2020-11-25 12:10:45 +0530

    auth service done

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { currentUser } from '../middlewares/current-user'
-import { requireAuth } from '../middlewares/require-auth'
 
 const router = express.Router();
 
-router.get("/api/users/currentuser",currentUser, requireAuth, (req: Request, res: Response) => {
-	return res.json({currentUser: req.currentUser || null})
+router.get("/api/users/currentuser",currentUser, (req: Request, res: Response) => {
+	res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
