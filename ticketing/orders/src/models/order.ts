5a1f268c8ff325812d80980680b9f7706d4677b6
jugaldb <jugalbhatt3@gmail.com>
2020-12-01 12:03:59 +0530

    typescript compile error

--- ticketing/orders/src/models/order.ts
+++ ticketing/orders/src/models/order.ts
@@ -50,7 +50,7 @@ const orderSchema = new mongoose.Schema(
   },
   {
     toJSON: {
-      transform(doc, ret) {
+      transform(doc: any, ret: any) {
         ret.id = ret._id;
         delete ret._id;
       },
