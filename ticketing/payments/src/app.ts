93b10da294b2b2a70a8e8daacd0b14d2c20c47a2
jugaldb <jugalbhatt3@gmail.com>
2020-12-03 10:33:01 +0530

    init payments routes

--- ticketing/payments/src/app.ts
+++ ticketing/payments/src/app.ts
@@ -1,21 +1,23 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError, currentUser } from '@jdbtickets/common';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@jdbtickets/common";
+import { newPaymentRouter } from "./routes/new";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 app.use(currentUser);
+app.use(newPaymentRouter);
 
-app.all('*', async (req, res) => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
